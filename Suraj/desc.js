48b0c56bb6eede3db758421d55ddf33401cdb4e3
palaksanghvi <palak.sanghvi@mindstix.com>
2015-06-05 18:53:27 +0530

    Descending order According to Score

---
+++ Suraj/desc.js
@@ -0,0 +1,50 @@
+var fs=require("fs");
+console.log("STUDENT DATABASE");
+var contents = fs.readFileSync("student.json");
+var cont = JSON.parse(contents);
+
+//console.log("contents : ",contents);
+var i=0;
+//console.log("FirstName" + "|" + "LastName" + "|" + "Score");
+var len = cont.mystudnt.length;
+
+				// for(i=0;i<len;i++)
+				// {
+				// 	console.log(cont.mystudnt[i].Firstname + "|" +
+				// 				cont.mystudnt[i].Lastname + "|" +
+		  // 						cont.mystudnt[i].Score
+				// 				);
+				// }
+//fs.writeFileSync("destination.txt","Firstname|Lastname|Score");
+var j=0;
+
+
+var stream = fs.createWriteStream("destination.txt");
+var arr=cont;
+stream.once('open',function(fd){
+
+	stream.write("FirstName" + "|" + "LastName" + "|" + "Score" );
+
+	for(i=0;i<len;i++)
+	{
+		for(j=i+1;j<len;j++)
+		{
+			if(cont.mystudnt[i].Score < cont.mystudnt[j].Score)
+			{
+					arr.mystudnt[0] = cont.mystudnt[j];
+					cont.mystudnt[j] = cont.mystudnt[i];
+					cont.mystudnt[i] = arr.mystudnt[0];
+
+
+		 			
+			}
+	    }
+	}
+	for(i=0;i<len;i++)
+	{
+					stream.write(cont.mystudnt[i].Firstname + "|" +
+					cont.mystudnt[i].Lastname + "|" +
+					cont.mystudnt[i].Score);
+	}
+	stream.end();
+});
